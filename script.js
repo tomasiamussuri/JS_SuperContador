10aaccff636bf7749f28f6ca70b20f54091fbbd0
tomasia.mussuri <tomasia.mussuri@gmail.com>
2022-11-06 23:54:04 -0300

    update js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function contar() {
-    let ini = document.getElementById("txti")
-    let fim = document.getElementById("txtf")
-    let passo = document.getElementById("txtp")
-    let res = document.getElementById("res")
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         res.innerHTML = 'Impossível contar.'
         window.alert('ERRO: Faltam dados!')
     } else {
-        res.innerHTML = "Contando: "
+        res.innerHTML = "Contando: </br>"
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
@@ -20,13 +20,13 @@ function contar() {
 
         if (i < f) {
             for(let c = i; c <= f; c += p) {
-                res.innerHTML += ` ${c} \u(1F449)`
+                res.innerHTML += ` ${c} \u{1F449}`
             }
         } else {
-            for(let c = i; c >= f; c-= p) {
-                res.innerHTML -= ` ${c} \u(1F449)`
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
             }
         }
-        res.innerHTML += `\U(1F3C1)`
+        res.innerHTML += `\u{1F3C1}`
     }
 }
